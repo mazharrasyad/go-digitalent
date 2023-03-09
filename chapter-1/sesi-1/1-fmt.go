a6a5433736d5f54c396883cbd787e0bc5021c021
Muhammad Azhar Rasyad <muhazharrasyad@gmail.com>
2023-03-09 22:35:05 +0700

    test

---
+++ chapter-1/sesi-1/1-fmt.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("<isi-pesan>")
+	fmt.Println("Hello World")
+	fmt.Println("Hello World", "Message", "From", "Go")
+
+	fmt.Println("Airell Jordan")
+	fmt.Println("Airell", "Jordan")
+
+	fmt.Print("Airell Jordan\n")
+	fmt.Print("Airell", " Jordan\n")
+	fmt.Print("Airell", " ", "Jordan\n")
+
+	comment()
+}
---
+++ chapter-1/sesi-1/2-comment.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func comment() {
+	fmt.Println("<isi-pesan> halo")
+}
